5697c4b1a685994b088900cdd1cb13d9c849de5d
Paolo Chang <chanhwan.chang@gmail.com>
2021-09-30 10:41:32 -0400

    Eject project and add webpack for server side rendering

---
+++ config/webpack.config.server.js
@@ -0,0 +1,129 @@
+const nodeExternals = require("webpack-node-externals");
+const paths = require("./paths");
+const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
+const webpack = require("webpack");
+const getClientEnvironment = require("./env");
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+
+module.exports = {
+  mode: "production",
+  entry: paths.ssrIndexJs,
+  target: "node",
+  output: {
+    path: paths.ssrBuild,
+    filename: "server.js",
+    chunkFilename: "js/[name].chunk/js",
+    publicPath: paths.publicUrlOrPath,
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          // Configuration for JS
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve("babel-loader"),
+            options: {
+              customize: require.resolve(
+                "babel-preset-react-app/webpack-overrides"
+              ),
+              plugins: [
+                [
+                  require.resolve("babel-plugin-named-asset-import"),
+                  {
+                    loaderMap: {
+                      svg: { ReactComponent: "@svgr/webpack?-svgo![path]" },
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+          // Configuration for CSS
+          {
+            test: cssRegex,
+            exclude: cssModuleRegex,
+            loader: require.resolve("css-loader"),
+            options: {
+              exportOnlyLocals: true,
+            },
+          },
+          // Configuration for CSS Module
+          {
+            test: cssModuleRegex,
+            loader: require.resolve("css-loader"),
+            options: {
+              modules: true,
+              exportOnlyLocals: true,
+              getLocalIdent: getCSSModuleLocalIdent,
+            },
+          },
+          // Configuration for Sass
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  exportOnlyLocals: true,
+                },
+              },
+              require.resolve("sass-loader"),
+            ],
+          },
+          // Configuration for Sass + CSS
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  modules: true,
+                  exportOnlyLocals: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                },
+              },
+              require.resolve("sass-loader"),
+            ],
+          },
+          // Configuration for url-loader
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              emitFile: false,
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+          // Use file-loader
+          {
+            loader: require.resolve("file-loader"),
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              emitFile: false,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ["node_modules"],
+  },
+  externals: [nodeExternals()],
+  plugins: [new webpack.DefinePlugin(env.stringified)],
+};
